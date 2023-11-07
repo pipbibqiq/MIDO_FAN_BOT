e34e65325cc79ed16349fed6b58a98b4589fcdf7
pipbibqiq <pipbibqiq@mail.ru>
2023-11-07 17:22:31 +0300

    ...

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def handle_button_1course_gss(callback_query: types.CallbackQuery):
     await callback_query.answer()  # Ответить на запрос обратного вызова, чтобы убрать "часики" у кнопки
     await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)  # Удалить старое сообщение с кнопками
     # Отправка фото из папки poit_1course
-    photo_path = "poit/poit_1course/poit_1course_schedule.jpg"  # Путь к вашему фото в папке poit_1course
+    photo_path = "poit/poit_1course/poit_1course_schedule.jpg"
     with open(photo_path, "rb") as photo_file:
         await bot.send_photo(callback_query.from_user.id, photo_file, caption="Общий лист расписания для 1 курса ПОИТ")
     await bot.send_message(callback_query.from_user.id, 'Выберите следующее действие:', reply_markup=ikb_midoPoit1CourseSchedule)
